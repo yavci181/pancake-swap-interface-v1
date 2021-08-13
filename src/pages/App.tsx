f1a52d5b25fa86e2e58ff55327d79a727b4c21b6
yavci181 <42972791+yavci181@users.noreply.github.com>
2021-08-13 14:45:56 +0000

    Update App.tsx

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -132,11 +132,18 @@ export default function App() {
                 <BodyWrapper>
                   <Popups />
                   <Web3ReactManager>
-                    <Switch>
+                 <Switch>
                       <Route exact strict path="/swap" component={Swap} />
+                      <Route exact strict path="/find" component={PoolFinder} />
+                      <Route exact strict path="/pool" component={Pool} />
+                      <Route exact path="/add" component={AddLiquidity} />
+                      <Route exact path="/migrate" component={Migration} />
+                      <Route exact strict path="/remove/:currencyIdA/:currencyIdB" component={RemoveLiquidity} />
 
                       {/* Redirection: These old routes are still used in the code base */}
-
+                      <Route exact path="/add/:currencyIdA" component={RedirectOldAddLiquidityPathStructure} />
+                      <Route exact path="/add/:currencyIdA/:currencyIdB" component={RedirectDuplicateTokenIds} />
+                      <Route exact strict path="/remove/:tokens" component={RedirectOldRemoveLiquidityPathStructure} />
 
                       <Route component={RedirectPathToSwapOnly} />
                     </Switch>
